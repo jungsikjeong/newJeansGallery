d10c5a516a5cae15e93517fb4493fad07e42bdc5
JungSik Jeong <wndtlr1024@gmail.com>
2023-04-20 05:33:43 +0900

    맨 위로 옮기는 버튼 css

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -246,7 +246,12 @@ const BackToTopBtn = styled.div`
     padding: 0.5rem;
     border: 1px solid #95a5a6;
     border-radius: 50%;
+    transition: all 0.3s ease;
     cursor: pointer;
+
+    &:hover {
+      transform: scale(1.1);
+    }
   }
   display: none;
 `;
