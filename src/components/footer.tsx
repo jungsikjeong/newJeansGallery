34e5cd780b84a99846466cc414c045d0dba7458c
jungsikjeong <wndtlr1024@gmail.com>
2024-06-09 23:21:37 +0900

    Styles: footer UI변경

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,109 +1,144 @@
-import { AiOutlineGithub } from 'react-icons/ai';
-import { BsInstagram } from 'react-icons/bs';
-import { FaFacebookSquare } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+import { AiOutlineGithub } from 'react-icons/ai';
+import { BsInstagram } from 'react-icons/bs';
+import { MdOutlineMailOutline } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+
 const Container = styled.section`
-  @media (min-width: 768px) {
-    padding: 2.5rem;
-    height: 25rem;
-  }
   background-color: #000;
   color: ${({ theme }) => theme.dullGray};
   padding: 1rem 2.5rem;
+  height: 25rem;
 `;
 
 const Wrapper = styled.div`
-  @media (min-width: 768px) {
-    max-width: 1200px;
-    position: relative;
-    margin: 0 auto;
-  }
+  max-width: 1200px;
+  position: relative;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
 
-  h1 {
-    font-size: 1.525rem;
-    padding: 2rem 0;
+const StyledText = styled.div`
+  display: flex;
+  font-size: 60px;
+  text-align: center;
+  color: #e13137;
+  position: relative;
+`;
+
+const Tspan1 = styled.span`
+  display: block;
+  color: #dfdfdf;
+  font-size: 60px;
+  position: relative;
+  top: -30px;
+`;
+
+const Tspan2 = styled.span`
+  display: block;
+  font-size: 100px;
+  position: relative;
+  top: 16px;
+  left: -14px;
+
+  @media (max-width: 768px) {
+    font-size: 68px;
   }
 `;
 
-const AddressWrap = styled.div`
-  @media (min-width: 768px) {
-    position: absolute;
-    top: 6rem;
-    left: 23rem;
-  }
-
-  p {
-    padding: 0.5rem 0;
+const Instructions = styled.div`
+  font-size: 0.9rem;
+  margin: 2rem 0;
+  @media (max-width: 768px) {
+    font-size: 0.785rem;
   }
 `;
 
 const IconWrapper = styled.div`
-  @media (min-width: 768px) {
-    position: absolute;
-    top: 3rem;
-    right: 0;
-  }
-  margin: 2.5rem 0;
-  font-size: 3rem;
-  h2 {
-    font-size: 1.25rem;
-    margin-bottom: 1rem;
-  }
-
-  .icon {
-    margin-right: 0.5rem;
-  }
+  margin-top: 2rem;
+  max-width: 450px;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.darkColor};
 `;
 
-const CopyRight = styled.div`
-  @media (min-width: 768px) {
-    position: absolute;
-    /* top: 6rem; */
-    left: 23rem;
-    bottom: -14rem;
-  }
-  margin-bottom: 1rem;
+const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+`;
 
-  p {
-    line-height: 1.5;
+const Copyright = styled.div`
+  margin-top: 2rem;
+
+  @media (max-width: 768px) {
+    font-size: 0.785rem;
   }
 `;
 
 const SLink = styled(Link)`
-  color: blue;
   text-decoration: underline;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
 const Footer = () => {
   return (
     <Container>
       <Wrapper>
-        <h1>GALLERIA</h1>
-        <AddressWrap>
-          <p>경기도 부천시 원미구</p>
-          <p>TEL 010-2222-2222</p>
-          <p>contact@galleria.com</p>
-        </AddressWrap>
+        <StyledText>
+          GA
+          <Tspan1>LL</Tspan1>
+          <Tspan2>ERIA</Tspan2>
+        </StyledText>
+        <Instructions>
+          이 홈페이지는{' '}
+          <SLink to='https://gallerya.netlify.app/'>Free Gallery</SLink>를
+          참고하여 제작하였습니다.
+        </Instructions>
 
         <IconWrapper>
-          <h2>Follow us</h2>
-          <BsInstagram className='icon' />
-          <AiOutlineGithub className='icon' />
-          <FaFacebookSquare className='icon' />
+          <SLink
+            to={`${process.env.REACT_APP_PUBLIC_INSTAGRAM}`}
+            target='_blank'
+            title='인스타 바로가기'
+          >
+            <Icon>
+              <BsInstagram />
+            </Icon>
+          </SLink>
+
+          <SLink
+            to={`${process.env.REACT_APP_PUBLIC_GITHUB}`}
+            target='_blank'
+            title='깃허브 바로가기'
+          >
+            <Icon>
+              <AiOutlineGithub />
+            </Icon>
+          </SLink>
+
+          <SLink to='mailto:wndtlr1024@gmail.com' title='메일 보내기'>
+            <Icon>
+              <MdOutlineMailOutline />
+            </Icon>
+          </SLink>
         </IconWrapper>
 
-        <CopyRight>
-          <p>
-            I copied the following design.
-            <br />
-            <SLink to='https://gallerya.netlify.app/#Greeting'>
-              FREE GALLERY
-            </SLink>
-            &nbsp;&nbsp;Thank you
-          </p>
-        </CopyRight>
+        <Copyright>Copyright © 2024 by Galleria, Inc.</Copyright>
       </Wrapper>
     </Container>
   );
